cea801f9ae962155b335026e60a961c6f3b7c6ea
raizengfilosofuxd <rickdavidsangalang@gmail.com>
2020-04-08 15:43:18 +0800

    Senc

---
+++ sketch_apr08b/sketch_apr08b.ino
@@ -0,0 +1,17 @@
+/* Use a photoresistor (or photocell) to turn on an LED in the dark
+   More info and circuit schematic: http://www.ardumotive.com/how-to-use-a-photoresistor-en.html
+   Dev: Michalis Vasilakis // Date: 8/6/2015 // www.ardumotive.com */
+   
+
+//Constants
+
+void setup(){
+ Serial.begin(9600);
+ 
+}
+
+void loop(){
+  
+  Serial.println("Yestung");
+  
+}
